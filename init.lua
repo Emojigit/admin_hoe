0a8c6f662b4e1e330b1b410fa4ea5c88d49ba43b
Yiu Man Ho <yiufamily.hh@gmail.com>
2020-08-25 15:37:37 +0800

    Publish

---
+++ init.lua
@@ -0,0 +1,6 @@
+farming.register_hoe("admin_hoe:hoe",{
+	description = "Admin Hoe",                      -- Description for tooltip
+	inventory_image = "farming_tool_steelhoe.png^[colorize:violet:50",  -- Image to be used as wield- and inventory image
+	max_uses = 32767,                         -- Uses until destroyed
+	material = "air",                         -- Material for recipes
+})
